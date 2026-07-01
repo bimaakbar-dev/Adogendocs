7aa92fc00509de6bbb74fbfb4293afa4366525ef
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-07-01 17:19:02 +0700

    Add files via upload

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,53 +1,41 @@
 //src/content.config.ts
-
+import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
 import { z } from "astro/zod";
-import { defineCollection } from "astro:content";
 
-const novels = defineCollection({
-  loader: glob({ base: "./src/content/novels", pattern: "**/*.{md,mdx}" }),
-  schema: z.object({
-    slug: z.string().optional(),
-    title: z.string(),
-    novelTitle: z.string(),
-    description: z.string().optional(),
-    pubDate: z.coerce.date().optional(),
-    order: z.number(),
-    cover: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }).optional(),
-    volume: z.string().optional(),
-    author: z.string().default("C0desk1"),
-    lastUpdated: z.coerce.date().optional(),
-  }),
+const METADATA = z.object({
+  slug: z.string().optional(),
+	title: z.string(),
+	description: z.string(),
+  pubDate: z.coerce.date().optional(),
+	lastUpdated: z.coerce.date().optional(),
+  cover: z.string().optional(),
+});
+
+const SEO = z.object({
+  title: z.string(),
+  description: z.string().optional(),
+  ogImage: z.string().optional(),
 });
 
 const docs = defineCollection({
   loader: glob({ base: "./src/content/docs", pattern: "**/*.{md,mdx}" }),
   schema: z.object({
-    slug: z.string(),
-    title: z.string(),
-    description: z.string().optional(),
+    ...METADATA.shape,
     category: z.string().optional(),
-    pubDate: z.coerce.date(),
     order: z.number().optional(),
-    lastUpdated: z.coerce.date().optional(),
+    seo: SEO,
   }),
 });
 
 const legal = defineCollection({
   loader: glob({ base: "./src/content/legal", pattern: "**/*.{md,mdx}" }),
   schema: z.object({
-    slug: z.string().optional(),
-    title: z.string(),
-    description: z.string().optional(),
-    lastUpdated: z.coerce.date().optional(),
+    ...METADATA.shape,
   }),
 });
 
 export const collections = {
-  novels,
   docs,
   legal,
 };
