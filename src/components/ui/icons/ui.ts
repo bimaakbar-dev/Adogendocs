bae922b166bb989d2c38ac6cf14c3aca4510f62b
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-07-04 14:32:04 +0700

    Update ui.ts

--- src/components/ui/icons/ui.ts
+++ src/components/ui/icons/ui.ts
@@ -359,19 +359,22 @@ export const uiIcons = {
   `,
   "chevron-right": `
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-	  <path d="M9 6l6 6l-6 6" />
+	<path d="M9 6l6 6l-6 6" />
   `,
   "chevron-left": `
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-	  <path d="M15 6l-6 6l6 6" />
+	<path d="M15 6l-6 6l6 6" />
   `,
   spinner: `
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M18 16v.01" />
-    <path d="M6 16v.01" />
-    <path d="M12 5v.01" />
-    <path d="M12 12v.01" />
-    <path d="M12 1a4 4 0 0 1 2.001 7.464l.001 .072a3.998 3.998 0 0 1 1.987 3.758l.22 .128a3.978 3.978 0 0 1 1.591 -.417l.2 -.005a4 4 0 1 1 -3.994 3.77l-.28 -.16c-.522 .25 -1.108 .39 -1.726 .39c-.619 0 -1.205 -.14 -1.728 -.391l-.279 .16l.007 .231a4 4 0 1 1 -2.212 -3.579l.222 -.129a3.998 3.998 0 0 1 1.988 -3.756l.002 -.071a4 4 0 0 1 -1.995 -3.265l-.005 -.2a4 4 0 0 1 4 -4" />
+    <path d="M12 6l0 -3" />
+    <path d="M16.25 7.75l2.15 -2.15" />
+    <path d="M18 12l3 0" />
+    <path d="M16.25 16.25l2.15 2.15" />
+    <path d="M12 18l0 3" />
+    <path d="M7.75 16.25l-2.15 2.15" />
+    <path d="M6 12l-3 0" />
+    <path d="M7.75 7.75l-2.15 -2.15" />
   `,
 
   more: `
