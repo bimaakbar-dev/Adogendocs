df294efa516c66b89e7dede0e382f2057e479ddc
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-07-05 20:30:55 +0700

    Update ui.ts

--- src/components/ui/icons/ui.ts
+++ src/components/ui/icons/ui.ts
@@ -78,8 +78,9 @@ export const uiIcons = {
   `,
 
   search: `
-    <circle cx="11" cy="11" r="8" />
-    <path d="m21 21-4.3-4.3" />
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+    <path d="M21 21l-6 -6" />
   `,
 
   menu: {
