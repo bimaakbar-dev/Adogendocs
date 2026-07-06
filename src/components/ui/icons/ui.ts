500a9d9838a7b9101ba44435b06e0be8768d0b7c
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-07-06 12:45:48 +0700

    Update ui.ts

--- src/components/ui/icons/ui.ts
+++ src/components/ui/icons/ui.ts
@@ -329,11 +329,9 @@ export const uiIcons = {
 
   share: `
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M3 12a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-    <path d="M15 6a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-    <path d="M15 18a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-    <path d="M8.7 10.7l6.6 -3.4" />
-    <path d="M8.7 13.3l6.6 3.4" />
+    <path d="M8 9h-1a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-8a2 2 0 0 0 -2 -2h-1" />
+    <path d="M12 14v-11" />
+    <path d="M9 6l3 -3l3 3" />
   `,
 
   "arrow-up": `<path d="M12 19V5m-7 7 7-7 7 7" />`,
