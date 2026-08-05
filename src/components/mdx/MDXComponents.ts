4771473cb72320acbb445d2fc226a306b66f0465
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-08-05 21:32:47 +0700

    Update MDXComponents.ts

--- src/components/mdx/MDXComponents.ts
+++ src/components/mdx/MDXComponents.ts
@@ -21,13 +21,6 @@ import Tabs from "./Tabs.astro";
 import CardGrid from "./CardGrid.astro";
 
 export const components = {
-  h1: H1,
-  h2: H2,
-  h3: H3,
-  h4: H4,
-  h5: H5,
-  h6: H6,
-  Callout,
   ChangelogContainer: Changelog,
   ChangelogItem: Changelog,
   Steps,
